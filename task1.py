149ebe4bbe480cb0e654fc91bd7710a4149f9aa3
Elena-star-sys <elenashulgaelena@gmail.com>
2024-07-24 15:36:02 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+#1st program
+print(9**0.5*5)
+
+#2and program
+print(9.99>9.98, 1000!=1000.1)
+
+#3rd program
+print(int(1234%1000)//10 + int((5678%1000)//10))
+
+#4th program
+print(int(13.42)  == 42.13 * 100 % 100) and (int(42.13)  == 13.42 * 100 % 100)
+
+
+
